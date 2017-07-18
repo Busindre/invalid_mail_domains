dfa3200587d6ec57368ab20bc12e2ec1e17aec29
Busindre <busilezas@gmail.com>
2017-07-18 13:23:05 +0200

    Update invalid_mail_domains.sh

--- invalid_mail_domains.sh
+++ invalid_mail_domains.sh
@@ -11,14 +11,10 @@
 
 [ -t 0 ] && { echo "Input expected from stdin."; exit 1; }
 dns=("8.8.4.4" "8.8.8.8" "208.67.222.222" "208.67.220.220" "8.26.56.26" "8.20.247.20") # DNS Servers to Randomize DNS Requests.
-stdin=$(</dev/stdin)
-extract=$(grep "status=deferred" <<< "$stdin" | grep -o "[[:space:]]to=[^[:space:]]*[[:space:]]" | sed -e "s/^[[:space:]]*to=<//" -e "s/>,[[:space:]]*$//"| sort | uniq | sed -e "s/^[^@]*@//"| sort | uniq) # Extract deferred domain names.
-IFS=$'\n' # Split on newline only.
-set -f    # Disable globbing.
-dominio=($(printf "%s" "$extract"))
-for fqdn_domain in "${dominio[@]}"; do	
-	(dig @${dns[$RANDOM % ${#dns[@]} ]} +noall +answer $fqdn_domain MX|grep MX &>/dev/null) || (nmap -Pn -p 25 $fqdn_domain 2>/dev/null | grep "tcp open" &>/dev/null || false) # If MX records are not found, port 25 of the domain is scanned.
-	if [ $? -eq 1 ]; then
-        	echo "$fqdn_domain"
-	fi
+extract=$(grep "status=deferred" | grep -o "[[:space:]]to=[^[:space:]]*[[:space:]]" | sed -e "s/^[[:space:]]*to=<//" -e "s/>,[[:space:]]*$//" -e "s/^[^@]*@//"| sort|uniq)
+for fqdn_domain in $extract; do
+        (dig @${dns[$RANDOM % ${#dns[@]} ]} +noall +answer $fqdn_domain MX|grep MX &>/dev/null) || (nmap -Pn -p 25 $fqdn_domain 2>/dev/null | grep "tcp open" &>/dev/null || false) # If MX records are not found, port 25 of the domain is scanned.
+        if [ $? -eq 1 ]; then
+                echo "$fqdn_domain"
+        fi
 done
